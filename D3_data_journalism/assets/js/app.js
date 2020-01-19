ddf6fe4853b63a77cd3576b2ca35380f5dd59287
Sanjiv Patel <sanj19@yahoo.com>
2020-01-18 21:54:03 -0500

    final

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,13 +1,13 @@
 // Define SVG area dimensions
 var svgWidth = 960;
-var svgHeight = 660;
+var svgHeight = 600;
 
 // Define the chart's margins as an object
 var chartMargin = {
     top: 30,
     right: 30,
-    bottom: 30,
-    left: 30
+    bottom: 60,
+    left: 50
 };
 
 // Define dimensions of the chart area
@@ -31,22 +31,22 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     console.log(censusData);
     // Cast the smokers value to a number for each piece of censusData
     censusData.forEach(function(d) {
-        d.age = +d.age;
-        d.obesity = +d.obesity;
+        d.poverty = +d.poverty;
+        d.healthcare = +d.healthcare;
 
-        console.log("x-axis", "age:", d.age);
-        console.log("y-axis", "obesity", d.obesity);
+        console.log("x-axis", "poverty:", d.poverty);
+        console.log("y-axis", "healthcare", d.healthcare);
     })
 
     // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
     var xLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(censusData, d => d.age)])
+      .domain([0, d3.max(censusData, d => d.poverty)])
       .range([0, chartWidth])
     ;    
 
     // Create a linear scale for the vertical axis.
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(censusData, d => d.obesity)])
+      .domain([0, d3.max(censusData, d => d.healthcare)])
       .range([chartHeight, 0])
     ;
 
@@ -62,32 +62,45 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
         .call(bottomAxis);
     
     chartGroup.append("g")
-        .call(leftAxis);
+		.call(leftAxis);
 
     // Create circles at each data point using data binding.
     var circlesGroup = chartGroup.selectAll("circle")
         .data(censusData)
         .enter()
         .append("circle")
-        .attr("cx", d => xLinearScale(d.age))
-        .attr("cy", d => yLinearScale(d.obesity))
-        .attr("r", "8")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "14")
         .attr("fill", "blue")
-        .attr("opacity", ".5");        
+		.attr("opacity", ".5")    
+
+    // Add text inside the circles
+	var circlesText = chartGroup.selectAll()
+        .data(censusData)
+        .enter()
+		.append("text")
+    	.attr("x", d => xLinearScale(d.poverty))
+    	.attr("y", d => yLinearScale(d.healthcare))
+    	.style("font-size", "9px")
+    	.style("text-anchor", "middle")
+    	.style("fill","white")
+		.text(d => (d.abbr))
+		.attr("dy", 3);  // shift text down a bit
 
     // Initialize the tool tip
     var toolTip = d3.tip()
-      .attr("class", "tooltip")
+      .attr("class", "d3-tip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.state}<br>Age: ${d.age}<br>Obesity: ${d.obesity}`);
+        return (`${d.state}<br> poverty: ${d.poverty} <br> healthcare: ${d.healthcare}`);
       });    
 
     // Create tooltip in the chart
     chartGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
-    circlesGroup.on("click", function(data) {
+    circlesGroup.on("mouseover", function(data) {
         toolTip.show(data, this);
       })
         // onmouseout event
@@ -98,16 +111,18 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     //Create Axes labels
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - chartMargin.left + 40)
+        .attr("y", 0 - chartMargin.left)
         .attr("x", 0 - (chartHeight / 2))
         .attr("dy", "1em")
         .attr("class", "axisText")
-        .text("Number of Billboard 100 Hits");    
+        .attr("font-weight", "bold")
+        .text("Lacks Healthcare (%)");    
 
-        chartGroup.append("text")
-        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 15})`)
         .attr("class", "axisText")
-        .text("Hair Metal Band Hair Length (inches)");
+        .text("In Poverty (%)")
+        .attr("font-weight", "bold");
     }).catch(function(error) {
         console.log(error);
 
